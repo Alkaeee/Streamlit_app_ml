d794b93b0d2c24c1cfe855c15e0f25b89ee769be
Javi de Alcazar <javierdealcazar98@gmail.com>
2023-11-30 02:08:56 +0100

    add

--- variables.py
+++ variables.py
@@ -17,7 +17,7 @@ URL_IMG_SNOW = "src/img/examples/snow.jpg"
 
 # DATA
 
-URL_SM_ACC_CSV = MAIN_PATH + "\\data\\datasets\\sm_acc.csv"
+URL_SM_ACC_CSV = "data\\datasets\\sm_acc.csv"
 
 
 # Analitycs
@@ -30,5 +30,5 @@ URL_HIST_MODEL_CNN = "src/img/analytics/final_model_history.png"
 URL_CONFUSION_MATRIX = "src/img/analytics/confusion_matrix_model.png"
 
 #MODELS
-URL_FINAL_MODEL = MAIN_PATH + "models\\deep_learning\\keras_models_2\\save_at_49.keras"
+URL_FINAL_MODEL = "models\\deep_learning\\keras_models_2\\save_at_49.keras"
 
