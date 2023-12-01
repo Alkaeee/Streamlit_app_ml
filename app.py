f183ab481556d4bca7d4fff4a1df0cbe412ae1fc
Javier Alcazar Mac <javierdealcazar98@gmail.com>
2023-12-01 11:02:39 +0100

    delete

--- app.py
+++ app.py
@@ -163,11 +163,7 @@ if ds:
         st.image(img_cm, width=550)
 
     st.markdown(an6, unsafe_allow_html=True)
-
-elif buss:
-    st.session_state.subtitle = "<h2>Análisis y conclusiones para negocio</h2>"
-
-    st.markdown(st.session_state.subtitle, unsafe_allow_html=True)
+j
 
 st.session_state.img_pred = st.file_uploader("Selecciona una imagen", type=["jpg", "jpeg", "png"])
 
